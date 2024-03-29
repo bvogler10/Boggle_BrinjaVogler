a3e1fac534a1c6f0633a6a3f31388b89b194e9ed
bvogler10 <brinjavogler@gmail.com>
2024-03-28 23:56:40 -0400

    got layout for top fragment and both fragments set up in main layout

---
+++ app/src/main/java/com/example/boggle/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.boggle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.FragmentTransaction
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val fragmentA = LetterChooseFragment()
+        val fragmentB = ScoreFragment()
+
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainerLetterChoose, fragmentA)
+            .replace(R.id.fragmentContainerScore, fragmentB)
+            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            .commit()
+    }
+}
